6570b74911798eb95a8e538f8524ed5da6520fc0
Matteo Hausner <matteo.hausner@gmail.com>
2019-10-04 01:46:28 +0200

    Added DCS F-16C profile

---
+++ configs/DCS/F-16C_50/vJoy Device.diff.lua
@@ -0,0 +1,305 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.25,
+						},
+						["deadzone"] = 0.15,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "Fliegen: Steuerknüppel, Höhenruder",
+		},
+		["a2002cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.25,
+						},
+						["deadzone"] = 0.15,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Fliegen: Steuerknüppel, Querruder",
+		},
+		["a2003cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.25,
+						},
+						["deadzone"] = 0.15,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_RZ",
+				},
+			},
+			["name"] = "Fliegen: Seitenruder",
+		},
+		["a2010cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RX",
+				},
+			},
+			["name"] = "Absolute Kamera, Sicht nach links/rechts drehen",
+		},
+		["a2011cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RY",
+				},
+			},
+			["name"] = "Absolute Kamera, Sicht nach vorne/hinten nicken",
+		},
+		["a2112cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+			["name"] = "Radbremse links",
+		},
+		["a2113cdnil"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_SLIDER2",
+				},
+			},
+			["name"] = "Radbremse rechts",
+		},
+	},
+	["keyDiffs"] = {
+		["d3014pnilu3014cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Gegenmaßnahmenschalter - Vorne",
+		},
+		["d3015pnilu3015cd16vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "Gegenmaßnahmenschalter - Hinten",
+		},
+		["d3016pnilu3016cd16vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "Gegenmaßnahmenschalter - Links",
+		},
+		["d3017pnilu3017cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Gegenmaßnahmenschalter - Rechts",
+		},
+		["d3018pnilu3018cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "Expand- / FOV-Knopf - Drücken",
+		},
+		["d3019pnilu3019cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Paddle-Schalter - Drücken",
+		},
+		["d3020pnilu3020cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Kamera- und Geschützabzug - Erste Drückstufe",
+		},
+		["d3021pnilu3021cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Kamera- und Geschützabzug - Zweite Drückstufe",
+		},
+		["d3022pnilu3022cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "WPN REL Knopf - Drücken",
+		},
+		["d3026pnilu3026cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "Transmit-Schalter - IFF OUT",
+		},
+		["d3027pnilu3027cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "Transmit-Schalter - IFF IN",
+		},
+		["d3043pnilunilcd16vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Luftkampf / Raketenüberbrückungsschalter - Drehen (Weiterschalten)",
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach oben rechts, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UR",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach unten rechts, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach unten links, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach oben links, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+		},
+		["dnilp3027unilcd2vdnilvp-0.3vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "YAW TRIM (Seitentrimmung) Regler - Gegen den Uhrzeigersinn / Links",
+		},
+		["dnilp3027unilcd2vdnilvp0.3vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "YAW TRIM (Seitentrimmung) Regler - Im Uhrzeigersinn / Rechts",
+		},
+		["dnilp3040unilcd16vdnilvp1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "MAN RNG Schalter - gegen den Uhrzeigersinn",
+		},
+		["dnilp3041unilcd16vdnilvp-1vunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "MAN RNG Schalter - im Uhrzeigersinn",
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach links, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach rechts, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht hoch (langsam)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "Sicht nach unten, langsam",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+	},
+}
+return diff
