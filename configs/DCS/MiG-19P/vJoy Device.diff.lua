5d13fae544832fbe6ca71f77c8febdb0607d5e6c
Matteo Hausner <matteo.hausner@gmail.com>
2020-04-29 23:19:29 +0200

    Completed DCS MiG-19P profile

--- configs/DCS/MiG-19P/vJoy Device.diff.lua
+++ configs/DCS/MiG-19P/vJoy Device.diff.lua
@@ -72,6 +72,70 @@ local diff = {
 		},
 	},
 	["keyDiffs"] = {
+		["d3317pnilu3317cd2vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Mil-power Limit/Afterburner Disable",
+		},
+		["d3318pnilu3318cd2vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "Afterburner Enable",
+		},
+		["d350pnilu351cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Bomb Pickle",
+		},
+		["d5005pnilunilcd11vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "ASP-5 Sight Cage - TOGGLE",
+		},
+		["d591pnilu592cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "ASP-5 Reticle Temporary Cage",
+		},
+		["d607pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "ASP-5 Target Wingspan Increase",
+		},
+		["d608pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "ASP-5 Target Wingspan Decrease",
+		},
+		["d84pnilu85cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Trigger",
+		},
 		["dnilp210u214cdnilvdnilvpnilvunil"] = {
 			["name"] = "Sicht nach oben rechts, langsam",
 			["removed"] = {
@@ -135,6 +199,22 @@ local diff = {
 					["key"] = "JOY_BTN_POV1_D",
 				},
 			},
+		},
+		["dnilp544unilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Optical Target Range Increase",
+		},
+		["dnilp545unilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "Optical Target Range Decrease",
 		},
 	},
 }
