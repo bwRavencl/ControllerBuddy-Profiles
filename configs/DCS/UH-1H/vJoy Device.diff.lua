3a1baef11bb0ab3f1584710d3348671050cf5a2e
Matteo Hausner <matteo.hausner@gmail.com>
2021-12-10 00:02:54 +0100

    DCS UH-1H profile: added missing bindings + descriptions

--- configs/DCS/UH-1H/vJoy Device.diff.lua
+++ configs/DCS/UH-1H/vJoy Device.diff.lua
@@ -72,30 +72,6 @@ local diff = {
 		},
 	},
 	["keyDiffs"] = {
-		["d3004pnilu3004cd41vd1vpnilvu0"] = {
-			["name"] = "Pilot Trimmer",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN3",
-				},
-			},
-		},
-		["d3009pnilu3009cd21vd1vpnilvu0"] = {
-			["name"] = "Pilot's radio trigger RADIO",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN1",
-				},
-			},
-		},
-		["d84pnilu84cdnilvd1vpnilvu0"] = {
-			["name"] = "Pilot weapon release/Machinegun fire",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN2",
-				},
-			},
-		},
 		["dnilp210u214cdnilvdnilvpnilvunil"] = {
 			["name"] = "View Up Right slow",
 			["removed"] = {
