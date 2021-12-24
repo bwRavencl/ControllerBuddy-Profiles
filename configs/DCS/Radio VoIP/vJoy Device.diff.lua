ae7a2f8174b40d1af4ed53a5fbf5a9ad9503ec6e
Matteo Hausner <matteo.hausner@gmail.com>
2021-12-24 02:32:54 +0100

    Resaved profiles with 0.20 file format - added VoIP radio bindings to DCS profiles

---
+++ configs/DCS/Radio VoIP/vJoy Device.diff.lua
@@ -0,0 +1,61 @@
+local diff = {
+	["keyDiffs"] = {
+		["d1731pnilu1731cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN100",
+				},
+			},
+			["name"] = "cockpit r.1 VoIP PTT",
+		},
+		["d1732pnilu1732cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN101",
+				},
+			},
+			["name"] = "cockpit r.2 VoIP PTT",
+		},
+		["d1737pnilu1738cdnilvd0.1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN104",
+				},
+			},
+			["name"] = "r.1 VoIP PTT",
+		},
+		["d1737pnilu1738cdnilvd0.2vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN105",
+				},
+			},
+			["name"] = "r.2 VoIP PTT",
+		},
+		["d1737pnilu1738cdnilvd0.3vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN106",
+				},
+			},
+			["name"] = "r.3 VoIP PTT",
+		},
+		["d1755pnilu1755cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN102",
+				},
+			},
+			["name"] = "cockpit r.3 VoIP PTT",
+		},
+		["d1756pnilu1756cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN103",
+				},
+			},
+			["name"] = "cockpit r.4 VoIP PTT",
+		},
+	},
+}
+return diff
