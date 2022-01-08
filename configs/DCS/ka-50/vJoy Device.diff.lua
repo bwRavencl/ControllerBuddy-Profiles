65bb0b83c315d0afa8f1fa2e3dd6a45c2a3dc91b
Matteo Hausner <matteo.hausner@gmail.com>
2022-01-08 14:07:49 +0100

    DCS KA-50 profile: updated profile and added descriptions

--- configs/DCS/ka-50/vJoy Device.diff.lua
+++ configs/DCS/ka-50/vJoy Device.diff.lua
@@ -108,22 +108,6 @@ local diff = {
 		},
 	},
 	["keyDiffs"] = {
-		["d350pnilu351cdnilvdnilvpnilvunil"] = {
-			["name"] = "Release weapons",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN2",
-				},
-			},
-		},
-		["d509pnilu510cdnilvdnilvpnilvunil"] = {
-			["name"] = "Lock target",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN4",
-				},
-			},
-		},
 		["d537pnilu385cdnilvdnilvpnilvunil"] = {
 			["name"] = "Hover On/Off",
 			["removed"] = {
@@ -132,22 +116,6 @@ local diff = {
 					["reformers"] = {
 						[1] = "LAlt",
 					},
-				},
-			},
-		},
-		["d84pnilu85cdnilvdnilvpnilvunil"] = {
-			["name"] = "Gun fire",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN1",
-				},
-			},
-		},
-		["d957pnilu958cdnilvdnilvpnilvunil"] = {
-			["name"] = "Trimmer",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_BTN3",
 				},
 			},
 		},
