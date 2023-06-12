cef374486e9d957d314285915d5364ea768aef7d
Matteo Hausner <matteo.hausner@gmail.com>
2023-06-12 18:41:27 +0200

    DCS A-4E config: adjusted roll curvature

--- configs/DCS/A-4E-C/vJoy Device.diff.lua
+++ configs/DCS/A-4E-C/vJoy Device.diff.lua
@@ -23,9 +23,12 @@ local diff = {
 				[1] = {
 					["filter"] = {
 						["curvature"] = {
-							[1] = 0.25,
+							[1] = 0.45,
 						},
 						["deadzone"] = 0.1,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
 						["invert"] = false,
 						["saturationX"] = 1,
 						["saturationY"] = 1,
