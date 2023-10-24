8d22e45105835d8b032a073245fccc46bd7fb75f
Matteo Hausner <matteo.hausner@gmail.com>
2023-10-24 18:33:49 +0200

    DCS Spitfire config: adjusted rudder curve

--- configs/DCS/SpitfireLFMkIXCW/vJoy Device.diff.lua
+++ configs/DCS/SpitfireLFMkIXCW/vJoy Device.diff.lua
@@ -41,9 +41,12 @@ local diff = {
 				[1] = {
 					["filter"] = {
 						["curvature"] = {
-							[1] = 0.1,
+							[1] = 0.25,
 						},
 						["deadzone"] = 0.25,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
 						["invert"] = false,
 						["saturationX"] = 1,
 						["saturationY"] = 1,
