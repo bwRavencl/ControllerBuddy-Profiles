4068ca5f01536bff59e8c07bcd46475d0a3bbfb1
Matteo Hausner <matteo.hausner@gmail.com>
2023-11-12 17:48:43 +0100

    DCS profiles: adjusted range of Absolute Camera axes to reduce dead bands

--- configs/DCS/F-16C_50/vJoy Device.diff.lua
+++ configs/DCS/F-16C_50/vJoy Device.diff.lua
@@ -57,6 +57,19 @@ local diff = {
 		["a2010cdnil"] = {
 			["added"] = {
 				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 0.41,
+						["slider"] = false,
+					},
 					["key"] = "JOY_RX",
 				},
 			},
@@ -65,6 +78,19 @@ local diff = {
 		["a2011cdnil"] = {
 			["added"] = {
 				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 0.5,
+						["slider"] = false,
+					},
 					["key"] = "JOY_RY",
 				},
 			},
--- configs/DCS/Ka-50 III/vJoy Device.diff.lua
+++ configs/DCS/Ka-50 III/vJoy Device.diff.lua
@@ -66,6 +66,19 @@ local diff = {
 		["a2010cdnil"] = {
 			["added"] = {
 				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 0.41,
+						["slider"] = false,
+					},
 					["key"] = "JOY_RX",
 				},
 			},
@@ -74,6 +87,19 @@ local diff = {
 		["a2011cdnil"] = {
 			["added"] = {
 				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 0.5,
+						["slider"] = false,
+					},
 					["key"] = "JOY_RY",
 				},
 			},
