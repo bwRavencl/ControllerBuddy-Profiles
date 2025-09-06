e5b1fb4765fa8042dc49ac24affcaa28444e0754
Matteo Hausner <matteo.hausner@gmail.com>
2025-09-06 02:20:24 +0200

    feat: invert supercharger axis of DCS F-4U-1D

--- configs/DCS/F4U-1D/vJoy Device.diff.lua
+++ configs/DCS/F4U-1D/vJoy Device.diff.lua
@@ -108,6 +108,19 @@ local diff = {
 		["a3235cd3"] = {
 			["added"] = {
 				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
 					["key"] = "JOY_SLIDER2",
 				},
 			},
